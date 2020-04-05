ed22074a6e22d954dd335c331a3d1d38a6445f25
izjing <izjing@izjingdeMacBook-Pro.local>
2020-04-06 00:04:01 +0800

    feat: 添加文章标题和内容

--- pages/home.vue
+++ pages/home.vue
@@ -1,10 +1,37 @@
 <template>
   <div>
-    <el-button>aaaaa</el-button>
+    <el-form ref="form" :model="formData" label-width="80px">
+      <el-form-item label="标题">
+        <el-input v-model="formData.title" />
+      </el-form-item>
+      <el-form-item label="内容">
+        <el-input v-model="formData.content" type="textarea" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">
+          立即创建
+        </el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      formData: {
+        title: '',
+        content: ''
+      }
+    }
+  },
+  methods: {
+    async onSubmit () {
+      const res = await this.$axios.post('/wenzhang', this.formData)
+      console.log(res)
+    }
+  }
 
 }
 </script>
