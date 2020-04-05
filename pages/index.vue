28d9c23cae3a9e7f98192386b5d89c5e054eedfa
izjing <izjing@izjingdeMacBook-Pro.local>
2020-04-06 00:04:25 +0800

    feat: 首页渲染文章

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,69 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-blogs
-      </h1>
-      <h2 class="subtitle">
-        My prime Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div v-infinite-scroll="load" class="home">
+    <div v-for="item in list" :key="item._id" class="home_img mrt15 shadow">
+      <el-image
+        style="width: 160px; height: 160px"
+        src="https://p.ananas.chaoxing.com/star3/origin/20fdd2116f0cc8c7f9f7360f71bd1ab1.jpg"
+      />
+      <div class="mrl20 flex1">
+        <h3>{{ item.title }}</h3>
+        <div class="mrt15">
+          {{ item.content }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
+  },
+  async asyncData (context) {
+    // console.log(context.$axios.get, 11122)
+    const res = await context.$axios.get('/findwenzhang', { params: { page: 1 } })
+    return {
+      list: res.data,
+      page: res.page
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    console.log(this.res)
+  },
+  methods: {
+    async load () {
+      let pages = this.page
+      pages++
+      const res = await this.$axios.get('/findwenzhang', { params: { page: pages } })
+      console.log(res)
+      const { code, data, page } = res
+      if (code === 1 && data.length > 0) {
+        this.page = page
+        this.list.push(...res.data)
+      }
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="less" scoped>
+  .home {
+    max-height: 1025px;
+    overflow: auto;
+    border-radius: 20px;
+    .home_img {
+      min-height: 200px;
+      padding: 15px;
+      border-radius: 20px;
+      border: 1px solid #e6e6e6;
+      background-color: #ffffff;
+      display: flex;
+    }
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
