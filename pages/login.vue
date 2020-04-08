fd79777ad830f80232b1e7bb107c787f6600fa26
izjing <53531436@qq.com>
2020-04-08 16:42:43 +0800

    feat: 移动端显示的登陆

---
+++ pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <userCard />
+</template>
+
+<script>
+import userCard from '~/components/userCard'
+
+export default {
+  name: 'Login',
+  components: {
+    userCard
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
