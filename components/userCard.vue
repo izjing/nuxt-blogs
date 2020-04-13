37b35dd30a0f6cf9b598c241ca86cdc505e554c3
izjing <53531436@qq.com>
2020-04-13 18:58:49 +0800

    feat: 增加邮箱验证码，调整校验

--- components/userCard.vue
+++ components/userCard.vue
@@ -16,19 +16,26 @@
         </el-button>
       </div>
       <!--    // 注册弹窗-->
-      <el-dialog title="注册账号" width="330px" :center="true" :visible.sync="dialogSignup">
-        <el-form :model="userSignup">
-          <el-form-item label="昵称" label-width="'120px'">
+      <el-dialog title="注册账号" width="450px" :center="true" :visible.sync="dialogSignup">
+        <el-form ref="dialogForm" :rules="rules" :model="userSignup">
+          <el-form-item label="昵称" label-width="'120px'" prop="name">
             <el-input v-model="userSignup.name" placeholder="请输入昵称" class="input_w" autocomplete="off" />
           </el-form-item>
-          <el-form-item label="账号" label-width="'120px'">
+          <el-form-item label="账号" label-width="'120px'" prop="userName">
             <el-input v-model="userSignup.userName" placeholder="请输入账号" class="input_w" autocomplete="off" />
           </el-form-item>
-          <el-form-item label="密码" label-width="'120px'">
+          <el-form-item label="密码" label-width="'120px'" prop="userPass">
             <el-input v-model="userSignup.userPass" placeholder="请输入密码" show-password class="input_w" autocomplete="off" />
           </el-form-item>
-          <el-form-item label="邮箱" placeholder="请输入邮箱" label-width="'120px'">
-            <el-input v-model="userSignup.userEmail" class="input_w" autocomplete="off" />
+          <el-form-item label="邮箱" label-width="'200px'" prop="userEmail">
+            <el-input v-model="userSignup.userEmail" placeholder="请输入邮箱" class="input_w" autocomplete="off" />
+            <el-button :disabled="!show" @click="send">
+              <span v-show="show">获取验证码</span>
+              <span v-show="!show" class="count">{{ count }} s</span>
+            </el-button>
+          </el-form-item>
+          <el-form-item label="验证码" label-width="'120px'" prop="authCode">
+            <el-input v-model="userSignup.authCode" placeholder="请输入验证码" class="input_w" autocomplete="off" />
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
@@ -67,7 +74,7 @@
 <script>
 import { mapState, mapActions, mapGetters, mapMutations } from 'vuex'
 // import { setToken } from '../assets/utils'
-
+const TIME_COUNT = 60
 export default {
   name: 'UserCard',
   data () {
@@ -80,8 +87,34 @@ export default {
       userSignup: {
         userName: '',
         userPass: '',
+        authCode: '',
         userEmail: ''
-      }
+      },
+      show: true,
+      count: 0,
+      timer: null,
+      rules: Object.freeze({
+        name: [
+          { required: true, message: '请输入昵称', trigger: 'blur' },
+          { min: 2, max: 8, message: '长度在 2 到 8 个字符', trigger: 'blur' }
+        ],
+        userName: [
+          { required: true, message: '请输入账号', trigger: 'blur' },
+          { min: 4, max: 16, message: '长度在 4 到 16 个字符', trigger: 'blur' }
+        ],
+        userPass: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }
+        ],
+        authCode: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { min: 6, max: 6, message: '长度在 6 个字符', trigger: 'blur' }
+        ],
+        userEmail: [
+          { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+          { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
+        ]
+      })
     }
   },
   computed: {
@@ -93,11 +126,20 @@ export default {
     })
   },
   methods: {
-    async signup () {
-      const res = await this.$axios.post('/signup', this.userSignup)
-      if (res.code === 1) {
-        this.dialogSignup = false
-      }
+    signup () {
+      this.$refs.dialogForm.validate(async (valid) => {
+        if (valid) {
+          const res = await this.$axios.post('/signup', this.userSignup)
+          if (res.code === 1) {
+            this.$message.success(res.msg)
+            this.dialogSignup = false
+            this.$refs.dialogForm.resetFields()
+          }
+        } else {
+          // console.log('error submit!!')
+          return false
+        }
+      })
     },
     ...mapActions('userInfo', {
       login: 'login'
@@ -105,6 +147,27 @@ export default {
     ...mapMutations('userInfo', {
       logOff: 'LOG_OFF'
     }),
+    // 发送邮箱验证码
+    async send () {
+      if (!this.timer) {
+        const res = await this.$axios.post('/loginMail', { email: this.userSignup.userEmail, userName: this.userSignup.userName })
+        const { code, msg } = res
+        if (code === 1) {
+          this.$message.success(msg)
+          this.count = TIME_COUNT
+          this.show = false
+          this.timer = setInterval(() => {
+            if (this.count > 0 && this.count <= TIME_COUNT) {
+              this.count--
+            } else {
+              this.show = true
+              clearInterval(this.timer) // 清除定时器
+              this.timer = null
+            }
+          }, 1000)
+        }
+      }
+    },
     async findUser () {
       await this.$axios.post('/aaa', {
         userName: 'aaaa'
